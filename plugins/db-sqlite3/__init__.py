c50b60a905f18f9f474d866ccd72bad08cbfba38
jacook <jacob@jcook.cc>
2013-07-03 17:58:39 -0400

    add sqlite3 database plugin

---
+++ plugins/db-sqlite3/__init__.py
@@ -0,0 +1,17 @@
+MODULES = ['main']
+
+DEPS =  [
+    (['any'],
+     [
+    	('app', 'sqlite', ''),
+     ])
+]
+
+NAME = 'Database - SQLite3'
+ICON = 'gen-database'
+PLATFORMS = ['any']
+DESCRIPTION = 'Add SQLite3 support to Databases'
+VERSION = '0'
+GENERATION = 1
+AUTHOR = 'arkOS'
+HOMEPAGE = 'http://arkos.io'
---
+++ plugins/db-sqlite3/main.py
@@ -0,0 +1,38 @@
+from genesis.api import *
+from genesis.ui import *
+from genesis.com import Plugin, Interface, implements
+from genesis import apis
+from genesis.utils import *
+
+import os
+
+
+class SQLite3(Plugin):
+    implements(apis.databases.IDatabase)
+    name = 'SQLite3'
+    icon = 'gen-database'
+
+    def add(self, dbname):
+        path = '/var/lib/sqlite3/%s.db' % dbname
+        status = shell_cs('sqlite3 %s "ATTACH \'%s\' AS %s;"' % (path,path,dbname), stderr=True)
+        if status[0] >= 1:
+            raise Exception(status[1])
+
+    def remove(self, dbname):
+        shell('rm /var/lib/sqlite3/%s.db' % dbname)
+
+    def adduser(self):
+        pass
+
+    def deluser(self):
+        pass
+
+    def execute(self, dbname, command):
+        return shell('sqlite3 /var/lib/sqlite3/%s.db "%s"' % (dbname, command), stderr=True)
+
+    def get_dbs(self):
+        dblist = []
+        for thing in os.listdir('/var/lib/sqlite3'):
+            if thing.endswith('.db'):
+                dblist.append({'name': thing.rstrip('.db'), 'type': 'SQLite3', 'class': self.__class__})
+        return dblist
