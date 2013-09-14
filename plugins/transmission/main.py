52885c9752a3ce9e3a8b531e74717cbc2f191fec
Will Wilson <wilson.william@gmail.com>
2013-09-14 10:15:19 -0400

    Convert to session variables.

--- plugins/transmission/main.py
+++ plugins/transmission/main.py
@@ -12,19 +12,19 @@ class TransmissionPlugin(apis.services.ServiceControlPlugin):
 
     def on_init(self):
         be = backend.TransmissionConfig(self.app)
-        self.config = be.load()
-        self.redir = False
+        self._config = be.load()
+        self._redir = False
 
     def on_session_start(self):
         pass
 
     def get_main_ui(self):
-        if(self.redir):
-            return self.redirapp(int(self.config['rpc-port']))
+        if self._redir:
+            return self.redirapp(int(self._config['rpc-port']))
         else:
             ui = self.app.inflate('transmission:main')
 
-            for k,v in sorted(self.config.items()):
+            for k,v in sorted(self._config.items()):
                 e = UI.DTR(
                     UI.IconFont(iconfont='gen-folder'),
                     UI.Label(text=k),
@@ -37,4 +37,4 @@ class TransmissionPlugin(apis.services.ServiceControlPlugin):
     @event('button/click')
     def on_click(self, event, params, vars = None):
         if params[0] == 'launch':
-            self.redir=True
+            self._redir=True
