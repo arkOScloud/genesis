42e0a9917ba5a44c7931479de2bbaf9f5550be91
Will Wilson <wilson.william@gmail.com>
2013-09-14 10:12:23 -0400

    Re-render main UI with redir

--- plugins/transmission/main.py
+++ plugins/transmission/main.py
@@ -13,24 +13,28 @@ class TransmissionPlugin(apis.services.ServiceControlPlugin):
     def on_init(self):
         be = backend.TransmissionConfig(self.app)
         self.config = be.load()
+        self.redir = False
 
     def on_session_start(self):
         pass
 
     def get_main_ui(self):
-        ui = self.app.inflate('transmission:main')
+        if(self.redir):
+            return self.redirapp(int(self.config['rpc-port']))
+        else:
+            ui = self.app.inflate('transmission:main')
 
-        for k,v in sorted(self.config.items()):
-            e = UI.DTR(
-                UI.IconFont(iconfont='gen-folder'),
-                UI.Label(text=k),
-                UI.Label(text=v),
-            )
-            ui.append('config', e)
+            for k,v in sorted(self.config.items()):
+                e = UI.DTR(
+                    UI.IconFont(iconfont='gen-folder'),
+                    UI.Label(text=k),
+                    UI.Label(text=v),
+                )
+                ui.append('config', e)
 
-        return ui
+            return ui
 
     @event('button/click')
     def on_click(self, event, params, vars = None):
         if params[0] == 'launch':
-            self.redirapp(int(self.config['rpc-port']))
+            self.redir=True
