73f3a324ad3b597ba0ad007d992a857332aa3589
jacook <jacob@jcook.cc>
2013-09-26 16:54:04 -0400

    build certificates plugin

--- genesis/plugins/certificates/backend.py
+++ genesis/plugins/certificates/backend.py
@@ -1,13 +1,59 @@
-import ntplib
+import ConfigParser
+import glob
 import OpenSSL
 import os
 
+from genesis import apis
+from genesis.com import *
+from genesis.utils import SystemTime
 from genesis.utils.error import SystemTimeError
+from genesis.plugins.core.api import ISSLPlugin
+from genesis.plugins.webapps.backend import WebappControl
 
+class CertControl(Plugin):
+	def get_certs(self):
+		# Find all certs added by Genesis and return basic information
+		certs = []
+		if not os.path.exists('/etc/ssl/certs/genesis'):
+			os.mkdir('/etc/ssl/certs/genesis')
+		if not os.path.exists('/etc/ssl/private/genesis'):
+			os.mkdir('/etc/ssl/private/genesis')
+		for x in glob.glob('/etc/ssl/certs/genesis/*.gcinfo'):
+			cfg = ConfigParser.ConfigParser()
+			cfg.read(x)
+			certs.append({'name': cfg.get('cert', 'name'),
+				'expiry': cfg.get('cert', 'expiry'),
+				'chain': cfg.get('cert', 'chain'),
+				'assign': cfg.get('cert', 'assign').split('\n')})
+		return certs
 
-class CertControl:
+	def get_ssl_capable(self):
+		lst = []
+		for x in apis.webapps(self.app).get_sites():
+			if x['ssl_able']:
+				lst.append(x)
+		return lst, self.app.grab_plugins(ISSLPlugin)
 
-	def gencert(certname, keyname):
+	def has_expired(self, certname):
+		# Return True if the plugin is expired, False if not
+		c = open('/etc/ssl/certs/genesis/'+certname+'.crt', 'r').read()
+		crt = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, c)
+		return crt.has_expired()
+
+	def add_ext_cert(self, name, chain='', assign=[]):
+		# Add a .gcinfo file for a certificate uploaded externally
+		# TODO accept and write file objs
+		c = open('/etc/ssl/certs/genesis/'+name+'.crt', 'r').read()
+		crt = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, c)
+		cfg = ConfigParser.ConfigParser()
+		cfg.add_section('cert')
+		cfg.set('cert', 'name', name)
+		cfg.set('cert', 'expiry', crt.get_notAfter())
+		cfg.set('cert', 'chain', chain)
+		cfg.set('cert', 'assigned', '\n'.join(assign))
+		cfg.write(open('/etc/ssl/certs/genesis/'+name+'.gcinfo', 'w'))
+
+	def gencert(self, name):
 		# Make sure our folders are in place
 		if not os.path.exists('/etc/ssl/certs/genesis'):
 			os.mkdir('/etc/ssl/certs/genesis')
@@ -15,10 +61,9 @@ class CertControl:
 			os.mkdir('/etc/ssl/private/genesis')
 
 		# If system time is way off, raise an error
-		ntp = ntplib.NTPClient()
-		resp = ntp.request('0.pool.ntp.org', version=3)
-		if resp.offset < -3600 or resp.offset > 3600:
-			raise SystemTimeError(resp.offset)
+		st = SystemTime().get_offset()
+		if st < -3600 or st > 3600:
+			raise SystemTimeError(st)
 
 		# Generate a key, then use it to sign a new cert
 		# We'll use 2048-bit RSA until pyOpenSSL supports ECC
@@ -29,14 +74,58 @@ class CertControl:
 		crt.gmtime_adj_notBefore(0)
 		crt.gmtime_adj_notAfter(2*365*24*60*60)
 		crt.set_pubkey(key)
-		crt.sign(k, 'sha1')
-		open(sslcert, "wt").write(
+		crt.sign(key, 'sha1')
+		open('/etc/ssl/certs/genesis/'+name+'.crt', "wt").write(
 			OpenSSL.crypto.dump_certificate(
-				OpenSSL.crypto.FILETYPE_PEM, 
-				'/etc/ssl/certs/genesis/'+crt)
+				OpenSSL.crypto.FILETYPE_PEM, crt)
 			)
-		open(sslkey, "wt").write(
+		os.chmod('/etc/ssl/certs/genesis/'+name+'.crt', 0660)
+		open('/etc/ssl/private/genesis/'+name+'.key', "wt").write(
 			OpenSSL.crypto.dump_privatekey(
-				OpenSSL.crypto.FILETYPE_PEM, 
-				'/etc/ssl/private/genesis/'+key)
+				OpenSSL.crypto.FILETYPE_PEM, key)
 			)
+		os.chmod('/etc/ssl/private/genesis/'+name+'.key', 0660)
+
+		cfg = ConfigParser.ConfigParser()
+		cfg.add_section('cert')
+		cfg.set('cert', 'name', name)
+		cfg.set('cert', 'expiry', crt.get_notAfter())
+		cfg.set('cert', 'chain', '')
+		cfg.set('cert', 'assign', '')
+		cfg.write(open('/etc/ssl/certs/genesis/'+name+'.gcinfo', 'w'))
+
+	def assign(self, name, assign):
+		# Assign a certificate to plugins/webapps as listed
+		# TODO read the cfg and disable SSL on unselected options
+		cfg = ConfigParser.ConfigParser()
+		cfg.read('/etc/ssl/certs/genesis/'+name+'.gcinfo')
+		alist = []
+		for x in assign:
+			if x[0] == 'genesis':
+				self.app.gconfig.set('genesis', 'cert_file', 
+					'/etc/ssl/certs/genesis/'+name+'.crt')
+				self.app.gconfig.set('genesis', 'cert_key', 
+					'/etc/ssl/private/genesis/'+name+'.key')
+				alist.append('Genesis')
+			elif x[0] == 'webapp':
+				WebappControl(self.app).ssl_enable(x[1],
+					'/etc/ssl/certs/genesis/'+name+'.crt',
+					'/etc/ssl/private/genesis/'+name+'.key')
+				alist.append(x[1]['name'] + ' ('+x[1]['type']+')')
+			elif x[0] == 'plugin':
+				x[1].enable_ssl()
+				alist.append(x[1].text)
+		cfg.set('cert', 'assign', '\n'.join(alist))
+		cfg.write(open('/etc/ssl/certs/genesis/'+name+'.gcinfo', 'w'))
+
+	def remove(self, name):
+		# Remove cert, key and control file for associated name
+		os.unlink('/etc/ssl/certs/genesis/'+name+'.gcinfo')
+		try:
+			os.unlink('/etc/ssl/certs/genesis/'+name+'.crt')
+		except:
+			pass
+		try:
+			os.unlink('/etc/ssl/private/genesis/'+name+'.key')
+		except:
+			pass
--- genesis/plugins/certificates/main.py
+++ genesis/plugins/certificates/main.py
@@ -4,6 +4,8 @@ from genesis.ui import *
 from genesis import apis
 from genesis.utils import *
 
+from backend import CertControl
+
 
 class CertificatesPlugin(CategoryPlugin):
 	text = 'Certificates'
@@ -11,30 +13,84 @@ class CertificatesPlugin(CategoryPlugin):
 	folder = 'system'
 
 	def on_init(self):
-		pass
+		self.certs = sorted(self._cc.get_certs(),
+			lambda x: x['name'])
 
 	def on_session_start(self):
-		pass
+		self._cc = CertControl(self.app)
+		self._gen = None
+		self._wal = []
+		self._pal = []
 
 	def get_ui(self):
 		ui = self.app.inflate('certificates:main')
+		lst = ui.find('certlist')
+
+		for s in self.certs:
+			lst.append(UI.DTR(
+				UI.IconFont(iconfont='gen-certificate'),
+				UI.Label(text=s['name']),
+				UI.Label(text=', '.join(s['assign'])),
+				UI.HContainer(
+					UI.TipIcon(iconfont='gen-info', text='Information',
+						id='info/' + str(self.certs.index(s))),
+					UI.TipIcon(iconfont='gen-close', text='Delete',
+						id='del/' + str(self.certs.index(s))),
+					),
+			   ))
+
+		if self._gen:
+			self._wal, self._pal = self._cc.get_ssl_capable()
+			for x in self._wal:
+				ui.find('certassign').append(
+					UI.Checkbox(text=x['name'], name='wassign[]', value=x['name'], checked=False),
+				)
+			for x in self._pal:
+				ui.find('certassign').append(
+					UI.Checkbox(text=x.text, name='passign[]', value=x.text, checked=False),
+				)
+		else:
+			ui.remove('dlgGen')
+
 		return ui
 
 	@event('button/click')
 	def on_click(self, event, params, vars = None):
 		if params[0] == 'add':
 			pass
+		if params[0] == 'gen':
+			self._gen = True
+		if params[0] == 'del':
+			self._cc.remove(self.certs[int(params[1])]['name'])
+			for a in self.certs[int(params[1])]['assign']:
+				pass
 
 	@event('dialog/submit')
 	def on_submit(self, event, params, vars = None):
 		if params[0] == 'dlgAdd':
 			if vars.getvalue('action', '') == 'OK':
 				pass
+		if params[0] == 'dlgGen':
+			if vars.getvalue('action', '') == 'OK':
+				lst = []
+				for i in range(0, len(self._wal)):
+					if vars.getvalue('wassign[]')[i] == '1':
+						lst.append(('webapp', self._wal[i]))
+				for i in range(0, len(self._pal)):
+					if vars.getvalue('passign[]')[i] == '1':
+						lst.append(('plugin', self._pal[i]))
+				cgw = CertGenWorker(self, vars.getvalue('certname'), lst)
+				cgw.start()
+			self._gen = False
 
 
 class CertGenWorker(BackgroundWorker):
 	def __init__(self, *args):
 		BackgroundWorker.__init__(self, *args)
 
-	def run(self):
-		pass
+	def run(self, cat, name, assign):
+		cat.put_statusmsg('Generating a certificate and key...')
+		CertControl(cat.app).gencert(name)
+		cat.put_statusmsg('Assigning new certificate...')
+		CertControl(cat.app).assign(name, assign)
+		cat.clr_statusmsg()
