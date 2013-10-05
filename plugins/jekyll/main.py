6625b7ea2321655f2d0800b0d078141d44df5418
jacook <jacob@jcook.cc>
2013-10-04 21:08:12 -0400

    add jekyll webapp plugin, closes #65

--- plugins/jekyll/main.py
+++ plugins/jekyll/main.py
@@ -5,6 +5,7 @@ from genesis import apis
 from genesis.utils import shell, download
 
 import re
+import os
 
 
 class Jekyll(Plugin):
@@ -19,14 +20,33 @@ class Jekyll(Plugin):
 	addtoblock = ''
 
 	def pre_install(self, name, vars):
+		# Make sure Ruby directory is in the PATH.
+		# This should work until there is a viable Ruby (RVM) plugin for Genesis
+		profile = []
+		f = open('/etc/profile', 'r')
+		for l in f.readlines():
+			if l.startswith('PATH="') and not 'ruby/2.0.0/bin' in l:
+				l = l.rstrip('"')
+				l += ':~/.gem/ruby/2.0.0/bin"'
+				profile.append(l)
+			else:
+				profile.append(l)
+		f.close()
+		open('/etc/profile', 'w').writelines(profile)
+
+		# Install the Jekyll and rdiscount gems required.
 		if not any('jekyll' in s for s in shell('gem list').split('\n')):
 			shell('gem install jekyll')
 		if not any('rdiscount' in s for s in shell('gem list').split('\n')):
 			shell('gem install rdiscount')
 
 	def post_install(self, name, path, vars):
+		# Make sure the webapps config points to the _site directory and generate it.
 		shell('sed -i "s/.*root .*/   root %s\;/" /etc/nginx/sites-available/%s' % (re.escape(path+'/_site'), name))
 		shell('jekyll build --source '+path+' --destination '+path+'/_site')
+
+		# Return an explicatory message.
+		return 'Jekyll has been setup, with a sample site at '+path+'. Modify these files as you like. To learn how to use Jekyll, visit http://jekyllrb.com/docs/usage. After making changes, click the Configure button next to the site, then "Regenerate Site" to bring your changes live.'
 
 	def pre_remove(self, name, path):
 		pass
@@ -39,6 +59,9 @@ class Jekyll(Plugin):
 
 	def ssl_disable(self, path):
 		pass
+
+	def regenerate_site(self, site):
+		shell('jekyll build --source '+site['path'].rstrip('_site')+' --destination '+os.path.join(site['path']))
 
 	def get_info(self):
 		return {
