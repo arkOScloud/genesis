d5860991fe09e98c26b23552ba8f0e49a9473737
peakwinter <jacob@jcook.cc>
2014-02-04 11:53:24 -0500

    finish current overhaul of filemanager plugin, closes #13

--- plugins/fm/config.py
+++ plugins/fm/config.py
@@ -1,6 +1,8 @@
 from genesis.api import ModuleConfig
 from main import *
 
+import os
+
 
 class GeneralConfig(ModuleConfig):
     target = FMPlugin
@@ -14,3 +16,7 @@ class GeneralConfig(ModuleConfig):
     dir = '/'
     showhidden = False
    
+    def __init__(self):
+        self.dir = os.path.expanduser('~%s' % self.app.auth.user) \
+        if hasattr(self.app, 'auth') and self.app.auth.user != 'anonymous' \
+        and os.path.isdir(os.path.expanduser('~%s' % self.app.auth.user)) else '/'
