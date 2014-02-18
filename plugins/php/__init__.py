f02a7c36c3baa13285bf05edb7254ac67daa9ec4
peakwinter <jacob@jcook.cc>
2014-02-18 17:18:50 -0500

    start php langassist plugin

---
+++ plugins/php/__init__.py
@@ -0,0 +1,24 @@
+# Plugin metadata
+NAME = 'PHP'
+TYPE = 'plugin'
+ICON = 'gen-code'
+DESCRIPTION = 'Change SSH settings and manage public keys'
+CATEGORIES = [
+    {
+        "primary": "Management",
+        "secondary": []
+    }
+]
+VERSION = '0'
+
+AUTHOR = 'arkOS'
+HOMEPAGE = 'http://arkos.io'
+LOGO = False
+
+SERVICES = []
+
+# Plugin parameters
+MODULES = ["main"]
+PLATFORMS = ["any"]
+DEPENDENCIES = {}
+GENERATION = 1
