86c6716652ab15c2b9f9149dd29f1b637fb85e4d
peakwinter <jacob@jcook.cc>
2014-03-20 12:01:08 -0400

    bump to 0.5.1

--- genesis/__init__.py
+++ genesis/__init__.py
@@ -6,7 +6,7 @@ def version():
     :returns: str
     """
 
-    release = '0.5'
+    release = '0.5.1'
     p = subprocess.Popen('git describe --tags 2> /dev/null',
             shell=True,
             stdout=subprocess.PIPE)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='genesis',
-    version='0.5',
+    version='0.5.1',
     install_requires=[
         'pyOpenSSL',
         'gevent',
