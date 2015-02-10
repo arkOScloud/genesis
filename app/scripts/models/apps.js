81bdee4bca2f3ed7e2d1b6aae174860fc735bd16
Jacob Cook <jacob@peakwinter.net>
2015-02-10 17:09:35 -0500

    flesh out app model

--- app/scripts/models/apps.js
+++ app/scripts/models/apps.js
@@ -1,4 +1,31 @@
 Genesis.App = DS.Model.extend({
     name: DS.attr('string'),
-    icon: DS.attr('string')
+    icon: DS.attr('string'),
+    error: DS.attr('boolean', {defaultValue: false}),
+    appAuthor: DS.attr('string'),
+    appHomepage: DS.attr('string'),
+    author: DS.attr('string'),
+    categories: DS.attr('string'),
+    dependencies: DS.attr(),
+    description: DS.attr(),
+    generation: DS.attr('number'),
+    homepage: DS.attr('string'),
+    loadable: DS.attr('boolean', {defaultValue: false}),
+    logo: DS.attr('boolean', {defaultValue: false}),
+    logoURL: function() {
+      return this.get('logo')?Genesis.Config.krakenHost+'/apps/logo/'+this.get('id'):null;
+    }.property('logo'),
+    modules: DS.attr(),
+    screenshots: DS.attr(),
+    services: DS.attr(),
+    type: DS.attr('string'),
+    version: DS.attr('string'),
+    database_engines: DS.attr(),
+    databaseMultiuser: DS.attr('boolean', {defaultValue: false}),
+    download_url: DS.attr('string'),
+    uses_php: DS.attr('boolean', {defaultValue: false}),
+    uses_ssl: DS.attr('boolean', {defaultValue: false}),
+    website_datapaths: DS.attr('boolean', {defaultValue: false}),
+    website_default_data_subdir: DS.attr('string'),
+    website_updates: DS.attr('boolean', {defaultValue: false})
 });
