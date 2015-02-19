651277cd89f58465c7352ceba5cc79c6a94bd003
Jacob Cook <jacob@peakwinter.net>
2015-02-19 16:30:29 -0500

    website addr from domains list

--- app/scripts/routes/websites.js
+++ app/scripts/routes/websites.js
@@ -2,6 +2,7 @@ Genesis.WebsitesRoute = Ember.Route.extend({
   model: function() {
     return Ember.RSVP.hash({
       sites: this.get('store').find('website'),
+      domains: this.get('store').find('domain'),
       apps: this.get('store').find('app', {type: "website", loadable: true})
     });
   },
