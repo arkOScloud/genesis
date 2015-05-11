8463dbcb159ee2210a2ca2f905d516c742a1178e
Jacob Cook <jacob@peakwinter.net>
2015-05-11 10:14:27 -0400

    bump to beta3

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@ var App = Ember.Application.extend({
   Resolver: Resolver
 });
 
-App.currentVersion = "0.7.0 Beta 2";
+App.currentVersion = "0.7.0 Beta 3";
 
 loadInitializers(App, config.modulePrefix);
 
