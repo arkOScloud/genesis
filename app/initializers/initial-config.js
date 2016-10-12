ac6de41da5ab9e28dfe31e328a09e2c26fdfe31d
Jacob Cook <jacob@peakwinter.net>
2016-10-11 20:52:40 -0400

    0.8.0

--- app/initializers/initial-config.js
+++ app/initializers/initial-config.js
@@ -5,10 +5,14 @@ import ENV from "../config/environment";
 export default {
   name: "initialConfig",
   initialize: function() {
-    Ember.$.getJSON(`${ENV.APP.krakenHost}/api/config`, function(j){
-      ENV.APP.dateFormat = j.config.general.date_format;
-      ENV.APP.timeFormat = j.config.general.time_format;
-      ENV.APP.needsFirstRun = !j.config.genesis.firstrun;
-    });
+    Ember.$.getJSON(`${ENV.APP.krakenHost}/api/config`)
+      .done(function(j) {
+        ENV.APP.dateFormat = j.config.general.date_format;
+        ENV.APP.timeFormat = j.config.general.time_format;
+        ENV.APP.needsFirstRun = !j.config.genesis.firstrun;
+      })
+      .fail(function() {
+        ENV.APP.needsFirstRun = false;
+      });
   }
 };
