afe02e7aed9c6a4013502f4fd8efd789834d31bb
Jacob Cook <jacob@peakwinter.net>
2016-10-31 16:08:24 -0400

    fixes #20

--- app/controllers/apps/info.js
+++ app/controllers/apps/info.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import handleModelError from '../../utils/handle-model-error';
+
 
 export default Ember.Controller.extend({
   breadCrumb: Ember.computed("model.name", "model.icon", {
@@ -9,6 +11,16 @@ export default Ember.Controller.extend({
   actions: {
     openModal: function(name) {
       Ember.$('.ui.' + name + '.modal').modal('show');
+    },
+    install: function() {
+      var app = this.get('model'),
+          self = this;
+      app.set('operation', 'install');
+      app.set('isReady', false);
+      var promise = app.save();
+      promise.then(function(){}, function(e){
+        handleModelError(self, e);
+      });
     },
     uninstallApp: function() {
       var app = this.get('model'),
